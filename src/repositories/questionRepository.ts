4b8efc0512da0bddb8cec70bc6bd61db313f78b3
MattGueler <mateus.gueler@gmail.com>
2022-09-08 20:49:28 -0300

    get question by id

--- src/repositories/questionRepository.ts
+++ src/repositories/questionRepository.ts
@@ -9,3 +9,10 @@ export async function getAllQuestions() {
   const allQuestions = await prisma.questions.findMany();
   return allQuestions;
 }
+
+export async function getQuestionById(questionId: number) {
+  const question = await prisma.questions.findFirst({
+    where: { id: questionId },
+  });
+  return question;
+}
