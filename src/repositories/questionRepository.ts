d4ef33fcb2b6689061088f36f205a87baed4f7ab
MattGueler <mateus.gueler@gmail.com>
2022-09-08 20:39:56 -0300

    get all questions

--- src/repositories/questionRepository.ts
+++ src/repositories/questionRepository.ts
@@ -4,3 +4,8 @@ import { IQuestions } from "../types/questionTypes";
 export async function insertQuestion(infos: IQuestions) {
   await prisma.questions.create({ data: infos });
 }
+
+export async function getAllQuestions() {
+  const allQuestions = await prisma.questions.findMany();
+  return allQuestions;
+}
--- src/services/questionService.ts
+++ src/services/questionService.ts
@@ -4,3 +4,8 @@ import * as questionRepository from "../repositories/questionRepository";
 export async function createQuestion(infos: IQuestions) {
   const newQuestion = await questionRepository.insertQuestion(infos);
 }
+
+export async function AllQuestionsExist() {
+  const allQuestions = await questionRepository.getAllQuestions();
+  return allQuestions;
+}
